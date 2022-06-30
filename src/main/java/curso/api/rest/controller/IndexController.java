ccebf1ea599adb17e500ff0f41ea507bd6657d74
anailson ribeiro <arsantos315@gmail.com>
2022-06-30 19:26:14 -0300

    Retornando usuário em JSON para a tela

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -1,5 +1,8 @@
 package curso.api.rest.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,17 +10,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import curso.api.rest.model.Usuario;
+
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
 	
 	/*SERVIÇO RESTFULL*/
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init(@RequestParam(value = "nome")String nome) {
+	public ResponseEntity<Usuario> init(){
 		
-		System.out.println("Parametro sendo recebido" + nome);
+		Usuario usuario = new Usuario();
+		usuario.setId(1L);
+		usuario.setLogin("teste");
+		usuario.setNome("Java");
+		usuario.setSenha("4464");
 		
-		return new ResponseEntity("Olá Rest Spring Boot" + nome , HttpStatus.OK);
+		Usuario usuario2 = new Usuario();
+		usuario2.setId(2L);
+		usuario2.setLogin("admin");
+		usuario2.setNome("admin");
+		usuario2.setSenha("4464");
+		
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+		usuarios.add(usuario);
+		usuarios.add(usuario2);
+		
+		return new ResponseEntity(usuarios, HttpStatus.OK);
+		
+		
 	}
 
 }
